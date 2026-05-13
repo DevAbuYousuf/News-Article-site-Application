8c3a9cc17ca1dc146b7c7d4375f4db81fad24bbe
YousufDev <mdyousuf3.ac@gmail.com>
2026-05-14 01:05:07 +0600

    Add paragraph element to App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
      <div className="flex min-h-svh flex-col items-center justify-center">
       <h1>hello World</h1>
       <Button className="px-4 py-6 text-red-600 hover:bg-primary-foreground">Click me</Button>
+      <p>This is a paragraph</p>
     </div>
   )
 }
