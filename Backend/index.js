902e9ce0877f2998faaf0aec15baae978e2690a5
YousufDev <mdyousuf3.ac@gmail.com>
2026-06-21 00:58:21 +0600

    initial-backend setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,17 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const app = express();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
